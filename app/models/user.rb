84e9dcfcfc805f52acaa92a5f23f2950ed8c0c66
PrimeRin <primedruk312@gmail.com>
2024-01-09 01:10:17 +0600

    add post ui design

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  has_one_attached :profile_pic
 end
