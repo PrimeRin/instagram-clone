4f6d822b28de657a1f6dd15c362c3dbd97715a62
PrimeRin <primedruk312@gmail.com>
2024-01-14 19:50:08 +0600

    add filepond and image preview lib

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap" # @5.3.2
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js" # @2.11.8
 
+pin "filepond" # @4.30.6
+pin_all_from "app/javascript/custom", under: "custom"
+pin "filepond-plugin-image-preview" # @4.6.12
