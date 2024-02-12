353233282a77c5865bfb32a5acd64ab3eb554d8e
PrimeRin <primedruk312@gmail.com>
2024-02-12 16:37:32 +0600

    added search user funtions

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'home#index'
   post "toggle_like", to:  "likes#toggle_like", as: :toggle_like
   resources :comments, only: [:create, :destroy]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   post "follow", to: 'follows#follow', as: :follow
   delete 'unfollow', to: 'follows#unfollow', as: :unfollow
   delete 'cancel_request', to: 'follows#cancel_request', as: :cancel_request
